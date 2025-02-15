b1aaa990daaed55e5995ccb4e126f65aa137efe2
Vibinson Raj G <vibinsonrajg2003@gmail.com>
2025-02-15 13:21:20 +0530

    updated commit

---
+++ src/assets/assets.js
@@ -0,0 +1,236 @@
+import bell_icon from './bell.png'
+import home_icon from './home.png'
+import like_icon from './like.png'
+import loop_icon from './loop.png'
+import mic_icon from './mic.png'
+import next_icon from './next.png'
+import play_icon from './play.png'
+import pause_icon from './pause.png'
+import plays_icon from './plays.png'
+import prev_icon from './prev.png'
+import search_icon from './search.png'
+import shuffle_icon from './shuffle.png'
+import speaker_icon from './speaker.png'
+import stack_icon from './stack.png'
+import zoom_icon from './zoom.png'
+import plus_icon from './plus.png'
+import arrow_icon from './arrow.png'
+import mini_player_icon from './mini-player.png'
+import queue_icon from './queue.png'
+import volume_icon from './volume.png'
+import arrow_right from './right_arrow.png'
+import arrow_left from './left_arrow.png'
+import spotify_logo from './logo.png'
+import clock_icon from './clock_icon.png'
+import img1 from './img1.jpg'
+import img2 from './img2.jpg'
+import img3 from './img3.jpg'
+import img4 from './img4.jpg'
+import img5 from './img5.jpg'
+import img6 from './img6.jpg'
+import img7 from './img7.jpg'
+import img8 from './img8.jpg'
+import img9 from './img9.jpg'
+import img10 from './img10.jpg'
+import img11 from './img11.jpg'
+import img12 from './img12.jpg'
+import img13 from './img13.jpg'
+import img14 from './img14.jpg'
+import img15 from './img15.jpg'
+import img16 from './img16.jpg'
+import song1 from  './song1.mp3'
+import song2 from  './song2.mp3'
+import song3 from  './song3.mp3'
+import song4 from  './song4.mp3'
+import song5 from  './song5.mp3'
+import song6 from  './song6.mp3'
+import song7 from  './song7.mp3'
+import song8 from  './song8.mp3'
+import uyirey from './uyirey.jpg'
+import heyminnale from './hey minnale.jpg'
+import kanave from './kanave.jpg'
+import vaanevaane from './vaane vaane.jpg'
+import vennilavusaaral  from './vennilavu saaral.jpg'
+import onandon from './On and On.jpg'
+import Elektronomia from './Elektronomia.jpg'
+import herostonight from './Heros Tonight.jpg'
+import Aasaorave from './Aasa Orave.mp3'
+import Chillanjirukkiye from './Chillanjirukkiye.mp3'
+import Manasilaayo from './Manasilaayo.mp3'
+import Manasilaayo1 from './Manasilaayo.jpg'
+import Aasaorave1 from './Aasa Orave.jpg'
+import Chillanjirukkiye1 from './Chillanjirukkiye.jpg'
+import nenjinezuth1 from './nenjin ezhuth.jpg'
+import nenjinezuth from './Nenjin Ezhuth.mp3'
+
+export const assets = {
+    bell_icon,
+    home_icon,
+    like_icon,
+    loop_icon,
+    mic_icon,
+    next_icon,
+    play_icon,
+    plays_icon,
+    prev_icon,
+    search_icon,
+    shuffle_icon,
+    speaker_icon,
+    stack_icon,
+    zoom_icon,
+    plus_icon,
+    arrow_icon,
+    mini_player_icon,
+    volume_icon,
+    queue_icon,
+    pause_icon,
+    arrow_left,
+    arrow_right,
+    spotify_logo,
+    clock_icon
+}
+
+
+export const albumsData = [
+    {   
+        id:0,
+        name: "Top 50 Global",
+        image: img8,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#2a4365"
+    },
+    {   
+        id:1,
+        name: "Top 50 India",
+        image: img9,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#22543d"
+    },
+    {   
+        id:2,
+        name: "Trending India",
+        image: img10,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#742a2a"
+    },
+    {   
+        id:3,
+        name: "Trending Global",
+        image: img16,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#44337a"
+    },
+    {   
+        id:4,
+        name: "Mega Hits,",
+        image: img11,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#234e52"
+    },
+    {   
+        id:5,
+        name: "Happy Favorites",
+        image: img15,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#744210"
+    }
+]
+
+export const songsData = [
+    {
+        id:0,
+        name: "On And On",
+        image: onandon,
+        file:song1,
+        desc:"by Cartoon, Jéja  ",
+        duration:"3:00"
+    },
+    {
+        id:1,
+        name: "Elektronomia",
+        image: Elektronomia,
+        file:song2,
+        desc:"by  Jakub Rene Kosik",
+        duration:"2:20"
+    },
+    {
+        id:2,
+        name: "Hero's Tonight",
+        image: herostonight,
+        file:song3,
+        desc:" by Janji, Johnning",
+        duration:"2:32"
+    },
+    {
+        id:3,
+        name: "Hey Minnale",
+        image: heyminnale,
+        file:song4,
+        desc:"by G.V. Prakash Kumar, Haricharan, Shweta Mohan",
+        duration:"2:50"
+    },
+    {
+        id:4,
+        name: "Kanave",
+        image: kanave,
+        file:song5,
+        desc:"by Yugabharathi, G.V. Prakash Kumar, Saindhavi",
+        duration:"3:10"
+    },
+    {
+        id:5,
+        name: "Uyirey",
+        image: uyirey    ,
+        file:song6,
+        desc:" by Vivek, G.V. Prakash Kumar, Nakul Abhyankar, Ramya Bhat Abhyankar",
+        duration:"2:45"
+    },
+    {
+        id:6,
+        name: "Vaane Vaane",
+        image: vaanevaane,
+        file:song7,
+        desc:"by G.V. Prakash Kumar, Faisal Razi, Yugabharathi",
+        duration:"2:18"
+    },
+    {
+        id:7,
+        name: "Vennilavu Saaral",
+        image: vennilavusaaral,
+        file:song8,
+        desc:"by Yugabharathi, G.V. Prakash Kumar, Kapil Kapilan, Rakshita Suresh",
+        duration:"2:35"
+    },
+    {
+        id:8,
+        name: "Aasaorave",
+        image: Aasaorave1,
+        file:Aasaorave,
+        desc:"by Yugabharathi, G.V. Prakash Kumar, Kapil Kapilan, Rakshita Suresh",
+        duration:"2:35"
+    },
+    {
+        id:9,
+        name: "Chillanjirukkiye",
+        image: Chillanjirukkiye1,
+        file:Chillanjirukkiye,
+        desc:"by Yugabharathi, G.V. Prakash Kumar, Kapil Kapilan, Rakshita Suresh",
+        duration:"2:35"
+    },
+    {
+        id:10,
+        name: "Manasilaayo",
+        image: Manasilaayo1,
+        file:Manasilaayo,
+        desc:"by Yugabharathi, G.V. Prakash Kumar, Kapil Kapilan, Rakshita Suresh",
+        duration:"2:35"
+    },
+    {
+        id:11,
+        name: "nenjin ezhuth",
+        image: nenjinezuth1,
+        file:nenjinezuth,
+        desc:"by Yugabharathi, G.V. Prakash Kumar, Kapil Kapilan, Rakshita Suresh",
+        duration:"2:35"
+    }
+]
